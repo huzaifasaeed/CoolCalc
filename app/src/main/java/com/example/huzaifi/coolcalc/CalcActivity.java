9c6c72f8f379d5feef1d9f4608800ccc37311e67
Huzaifa Saeed <huzaifasaeed@users.noreply.github.com>
2017-11-04 17:57:09 +0500

    Initial commit

---
+++ app/src/main/java/com/example/huzaifi/coolcalc/CalcActivity.java
@@ -0,0 +1,13 @@
+package com.example.huzaifi.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
